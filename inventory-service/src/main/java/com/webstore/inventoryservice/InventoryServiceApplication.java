e2110d38c06e4f88a6d6e49f4b430bc026c0f61b
joao <joaovaccorci@gmail.com>
2024-02-02 11:21:14 -0300

    Feat: Add Inventory Service and Change DB Name Order Service

---
+++ inventory-service/src/main/java/com/webstore/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.webstore.inventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+}
---
+++ inventory-service/src/test/java/com/webstore/inventoryservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webstore.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
